3be5cc0f65c548adc32295332c386caa86e8e354
Mehdi GHESH <mehdi.ghesh@hotmail.fr>
2023-09-29 12:57:40 +0200

    fix: use RequestInit from node-fetch instead of inbuilt typescript
    Fix based on this stackoverflow answer https://stackoverflow.com/a/67870352/5868851

--- src/utils/auth.util.ts
+++ src/utils/auth.util.ts
@@ -1,11 +1,14 @@
 import { SonarQubeSDKAuth } from '../interfaces';
+import { RequestInit } from 'node-fetch'
 
 export class AuthUtils {
   static setAuthInHeadersIfConfigured = (auth?: SonarQubeSDKAuth) => {
-    const options: Record<string, string> = {};
+    const options: RequestInit = {};
     if (auth) {
-      options['Authorization'] =
-        'Basic ' + this.getBaseEncodedAuthCredentials(auth);
+      const headersInit: HeadersInit = {};
+      headersInit.Authorization =
+      'Basic ' + this.getBaseEncodedAuthCredentials(auth);
+      options.headers = headersInit;
     }
     return options;
   };
